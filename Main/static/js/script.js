16b2cc23abb253b6f902f444aa409d0ff09f1039
Mithun Raj <kmithunraj02@gmail.com>
2023-02-24 20:04:26 +0530

    Profile page - cancel button bug fixed

--- Main/static/js/script.js
+++ Main/static/js/script.js
@@ -43,7 +43,20 @@ document.addEventListener('mouseup', function(e) {
     }
 });
 
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 5000);
+}
 
 function profileEnable() {
 document.querySelectorAll('input[disabled]').forEach(input => input.removeAttribute('disabled'));
